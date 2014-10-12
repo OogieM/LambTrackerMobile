2f72c4f547191890b8e17c46cfa186f837f95efa
Oogie McGuire <oogiem@desertweyr.com>
2014-10-12 07:47:50 -0600

    Removed the time from all note display fields to save screen space. Starting to test options for landscape screen stuff for some activities in test interface designs activity but it's not working properly.

--- LambTracker_V2/src/com/weyr_associates/lambtracker/LookUpSheep.java
+++ LambTracker_V2/src/com/weyr_associates/lambtracker/LookUpSheep.java
@@ -506,7 +506,8 @@ public void formatSheepRecord (View v){
     	showAlert(v);
 	}
 	//	Now go get all the notes for this sheep and format them
-	cmd = String.format( "select sheep_note_table.id_noteid as _id, sheep_note_table.note_date, sheep_note_table.note_time, " +
+	cmd = String.format( "select sheep_note_table.id_noteid as _id, sheep_note_table.note_date, " +
+	// cmd = String.format( "select sheep_note_table.id_noteid as _id, sheep_note_table.note_date, sheep_note_table.note_time, " +
 			"sheep_note_table.note_text, predefined_notes_table.predefined_note_text " +
 			" from sheep_note_table left join predefined_notes_table " +
 			"on predefined_notes_table.id_predefinednotesid = sheep_note_table.id_predefinednotesid01" +
@@ -520,11 +521,20 @@ public void formatSheepRecord (View v){
 	cursor4.moveToFirst();	
 	if (nRecs4 > 0) {
     	// format the note records
+		// Pulled this from the note entry xml file to remove the time from a note. Can be added back if needed
+//		<TextView
+//        android:id="@+id/note_time"
+//        android:layout_width="wrap_content"
+//        android:layout_height="wrap_content"
+//        android:textSize="14sp" />
 		//	Select only the columns I need for the note display section
-    	String[] fromColumns2 = new String[ ]{ "note_date", "note_time", "note_text", "predefined_note_text"};
+//    	String[] fromColumns2 = new String[ ]{ "note_date", "note_time", "note_text", "predefined_note_text"};
+    	String[] fromColumns2 = new String[ ]{ "note_date", "note_text", "predefined_note_text"};
 		Log.i("LookForSheep", "after setting string array fromColumns for notes");
 		//	Set the views for each column for each line. A tag takes up 1 line on the screen
-		int[] toViews2 = new int[] { R.id.note_date, R.id.note_time, R.id.note_text, R.id.predefined_note_text};
+		//Removed the time from a note
+//		int[] toViews2 = new int[] { R.id.note_date, R.id.note_time, R.id.note_text, R.id.predefined_note_text};
+		int[] toViews2 = new int[] { R.id.note_date, R.id.note_text, R.id.predefined_note_text};
         Log.i("LookForSheep", "after setting string array toViews for notes");
         myadapter2 = new SimpleCursorAdapter(this, R.layout.note_entry, cursor4 ,fromColumns2, toViews2, 0);
         Log.i("LookForSheep", "after setting myadapter to show notes");
